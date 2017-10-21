dda14284481cb2918256f48b8707488a0a47d5ad
MrAubergine <chris@jackson.me.uk>
2017-10-21 19:26:53 +0100

    Basic implementation of UI with test button, and start of implementation of BT comms classes

---
+++ AlexLights/AlexLights/Comms/LightDriver.cs
@@ -0,0 +1,108 @@
+﻿using Android.Bluetooth;
+using Java.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlexLights.Comms
+{
+    class LightDriver
+    {
+        private static volatile LightDriver instance;
+        private static object syncRoot = new Object();
+
+        private BluetoothAdapter btAdapter = null;
+        private BluetoothDevice btDevice = null;
+        private UUID sppUUID = null;
+        private BluetoothSocket btSocket = null;
+        private bool btConnected = false;
+
+        private bool connected;
+
+        public static LightDriver Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new LightDriver();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        private LightDriver() { }
+
+        private bool Connect()
+        {
+            if (btAdapter == null)
+            {
+                btAdapter = BluetoothAdapter.DefaultAdapter;
+                if (btAdapter == null)
+                    return false;
+            }
+
+            if (btDevice == null)
+            {
+                var devices = btAdapter.BondedDevices;
+                foreach (BluetoothDevice device in devices)
+                {
+                    if (device.Name == "Adafruit EZ-Link a131")
+                    {
+                        btDevice = device;
+                        break;
+                    }
+                }
+                if (btDevice == null)
+                    return false;
+            }
+
+            if (sppUUID == null)
+            {
+                sppUUID = UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
+            }
+
+            if (btSocket == null)
+            {
+                btSocket = btDevice.CreateInsecureRfcommSocketToServiceRecord(sppUUID);
+            }
+            if (btSocket == null)
+                return false;
+
+            try
+            {
+                btSocket.Connect();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void Close()
+        {
+            if (btSocket != null)
+            {
+                btSocket.Close();
+            }
+        }
+
+        public bool SendString(string str)
+        {
+            if (!Connect())
+                return false;
+
+            btSocket.OutputStream.WriteByte(42);
+            Close();
+
+            return true;
+        }
+    }
+}
---
+++ AlexLights/AlexLights/Pages/SolidColourPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using AlexLights.Comms;
+using Android.Bluetooth;
+using Java.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AlexLights.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SolidColourPage : ContentPage
+	{
+		public SolidColourPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void OnBoopClicked(object sender, EventArgs e)
+        {
+            LightDriver.Instance.SendString("");
+        }
+    }
+}
